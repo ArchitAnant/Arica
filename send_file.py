4b1d0affcbb2d69afb9a91104a3efb283771d8cd
Archit Anant <architanant5@gmail.com>
2024-03-10 19:30:19 +0530

    fix: send_file

--- send_file.py
+++ send_file.py
@@ -6,11 +6,12 @@ def send_file():
     print("Sending file...")
     file_name = input("Enter the file name: ")
     port_no = input("Enter the port number: ")
+    ip = input("Enter the IP address: ")
     #check if port is empty
     if port_no == "":
         port_no = "9999"
     #send file over netcat
-    sb.run("nc -l {} < {}".format(file_name,int(port_no)),shell=True)
+    sb.run("netcat -w 1 {} {} < {}".format(ip,int(port_no),file_name),shell=True)
     time.sleep(10)
     print("File sent successfully")
     # close the netcat connection
@@ -25,7 +26,7 @@ def receive_file():
     if port_no == "":
         port_no = "9999"
     #receive file over netcat
-    sb.run("nc -l {} > {}".format(port_no,file_name),shell=True)
+    sb.run("netcat -lnvp {} > {}".format(port_no,file_name),shell=True)
     time.sleep(10)
     print("File received successfully")
     # close the netcat connection
